edb38b1f547a1ec4dc1a3f8316e813e97cbce37a
Luis <44007026+LAraujo7@users.noreply.github.com>
2022-02-14 23:19:49 +0000

    File Organization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-
-import DVDLogo from "./DVDLogo";
+import SEILogo from "./SEILogo";
 
 const svgWidth = window.innerWidth;
 const svgHeight = window.innerHeight;
 
 export default function App() {
   return (
-    <div className="w-full h-full bg-black overflow-hidden">
+    <div className="w-full h-full bg-primary overflow-hidden">
       <svg width={svgWidth} height={svgHeight}>
-        <DVDLogo width={svgWidth} height={svgHeight}/>
+        <SEILogo width={svgWidth} height={svgHeight}/>
       </svg>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#011A2C",
+        secondary: "#01253D",
+        tertiary: "#063D66",
+        quaternary: "#0085A6",
+        quinary: "#36DBEE",
+        success: "#008F05",
+        failure: "#FF4444",
+        warning: "#E09200",
+      },
+    }
   },
   plugins: [],
 }
