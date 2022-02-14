04fb31538837d9bdc554ec940b23b262084d66ea
Luis <44007026+LAraujo7@users.noreply.github.com>
2022-02-14 23:49:44 +0000

    Add black bg

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const svgHeight = window.innerHeight;
 
 export default function App() {
   return (
-    <div className="w-full h-full bg-primary overflow-hidden">
+    <div className="w-full h-full overflow-hidden bg-black">
       <svg width={svgWidth} height={svgHeight}>
         <SEILogo width={svgWidth} height={svgHeight}/>
       </svg>
